5769643957319c9af4390dd459ff63eb0bb1661d
JGerdes <jonas.gerdes@gmx.net>
2014-03-25 20:05:07 +0100

    Whiteboard und WhiteboardEntry hinzugefügt

--- MdsCommon/src/de/hsbremen/mds/android/whiteboard/Whiteboard.java
+++
@@ -1,8 +0,0 @@
-package de.hsbremen.mds.android.whiteboard;
-
-public class Whiteboard {
-
-	public ItemList itemList;
-	public WhiteboardPlayer players;
-	
-}
---
+++ MdsCommon/src/de/hsbremen/mds/common/whiteboard/Whiteboard.java
@@ -0,0 +1,17 @@
+package de.hsbremen.mds.common.whiteboard;
+
+import java.util.HashMap;
+/**
+ * 
+ * Das Whiteboard ist eine HashMap, die einem String (key) einen Eintrag in
+ * das Whiteboard (WhiteboardEntry) zuordnet.
+ * 
+ * @author Interpreter
+ *
+ */
+public class Whiteboard extends HashMap<String, WhiteboardEntry>{
+
+	
+	
+
+}

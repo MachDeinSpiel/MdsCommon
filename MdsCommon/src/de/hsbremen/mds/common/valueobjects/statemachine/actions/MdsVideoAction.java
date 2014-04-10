141264044135e8b83f508b39032d788dbe0d0c9b
JGerdes <jonas.gerdes@gmx.net>
2014-04-10 19:16:29 +0200

    null in die Prüfung hinzugefügt

--- MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsVideoAction.java
+++ MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsVideoAction.java
@@ -14,11 +14,11 @@ public class MdsVideoAction implements MdsActionExecutable {
 	public MdsVideoAction(String title, String url, String text) {	
 		
 		// set Name
-		String i = title.equals("") ? title : title;
+		String i = (title.equals("") || title == null) ? title : title;
 		// set URL
-		String u = url.equals("") ? videoNotFoundURL : url;
+		String u = (url.equals("") || url == null) ? videoNotFoundURL : url;
 		// set Text
-		String t = text.equals("") ? textNotFound : text;
+		String t = (text.equals("")|| text == null) ? textNotFound : text;
 		vid = new MdsVideo(title, u, t);
 	}
 

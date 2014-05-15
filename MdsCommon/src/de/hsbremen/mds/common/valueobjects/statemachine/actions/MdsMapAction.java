cc4ea3015aac5616d93abf42e34cd80219f25602
JGerdes <jonas.gerdes@gmx.net>
2014-05-15 18:38:24 +0200

    MdsMapAction implementiert

--- MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsMapAction.java
+++ MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsMapAction.java
@@ -1,6 +1,7 @@
 package de.hsbremen.mds.common.valueobjects.statemachine.actions;
 
 import de.hsbremen.mds.common.interfaces.GuiInterface;
+import de.hsbremen.mds.common.valueobjects.MdsMap;
 
 // TODO: Vlt gar nicht benötigt?
 
@@ -12,7 +13,8 @@ public class MdsMapAction implements MdsActionExecutable {
 
 	@Override
 	public void execute(GuiInterface guiInterface) {
-		// TODO Auto-generated method stub
+		//TODO: Parameter?
+		guiInterface.nextFragment(new MdsMap("Whatever", "belongs", "here"));
 
 	}
 

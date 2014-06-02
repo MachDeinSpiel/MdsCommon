c7bbaf8951d2318b0fb3499cb37e83916442cf32
nhatje <nhatje@gmx.de>
2014-06-02 23:35:38 +0200

    MdsMapAction geändert

--- MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsMapAction.java
+++ MdsCommon/src/de/hsbremen/mds/common/valueobjects/statemachine/actions/MdsMapAction.java
@@ -3,19 +3,26 @@ package de.hsbremen.mds.common.valueobjects.statemachine.actions;
 import de.hsbremen.mds.common.interfaces.GuiInterface;
 import de.hsbremen.mds.common.valueobjects.MdsMap;
 import de.hsbremen.mds.common.valueobjects.statemachine.MdsInfoObject;
+import de.hsbremen.mds.common.valueobjects.statemachine.MdsInfoObject.InfoType;
 
 // TODO: Vlt gar nicht benötigt?
 
 public class MdsMapAction implements MdsActionExecutable {
+	
+	public MdsInfoObject map;
 
 	public MdsMapAction(String ident, double lon, double lat) {
-		// TODO Auto-generated constructor stub
+		
+		// set Name
+		String i = ident;
+				
+		map = new MdsInfoObject(InfoType.MdsImage, i, null, null, null);
 	}
 
 	@Override
 	public void execute(GuiInterface guiInterface) {
 		//TODO: Parameter?
-		guiInterface.nextFragment(new MdsInfoObject(MdsInfoObject.InfoType.MdsMap, "Whatever", "belongs", "here", null));
+		guiInterface.nextFragment(map);
 
 	}
 

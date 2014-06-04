8a0eea7793197528cf67ad16110462d974fff176
nhatje <nhatje@gmx.de>
2014-06-04 22:38:17 +0200

    Standart visibility ist nun "all"

--- MdsCommon/src/de/hsbremen/mds/common/whiteboard/WhiteboardEntry.java
+++ MdsCommon/src/de/hsbremen/mds/common/whiteboard/WhiteboardEntry.java
@@ -20,7 +20,7 @@ public class WhiteboardEntry implements Serializable {
 	private static final long serialVersionUID = -8411200425558570022L;
 	
 	public Object value;
-	public String visibility;
+	public String visibility = "all";
 	
 	public WhiteboardEntry(Object value, String visibility) throws InvalidWhiteboardEntryException{
 		if(!(value instanceof String || value instanceof Whiteboard)){

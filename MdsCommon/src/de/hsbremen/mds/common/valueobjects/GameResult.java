3487c6b8dd7743278559972217ca5a9f7e5ec1be
nhatje <nhatje@gmx.de>
2014-06-27 12:29:30 +0200

    minScore in GameResult

--- MdsCommon/src/de/hsbremen/mds/common/valueobjects/GameResult.java
+++ MdsCommon/src/de/hsbremen/mds/common/valueobjects/GameResult.java
@@ -5,16 +5,18 @@ public class GameResult {
 	public String attribute;
 	public String setWin = null;
 	public String setLoose = null;
-	public double addResult = 0;
+	public String addResult = null;
 	public double factor = 1;
+	public int minScore = -10;
 	
-	public GameResult(String attribute, String setWin, String setLoose, double addResult, double factor) {
+	public GameResult(String attribute, String setWin, String setLoose, String addResult, double factor, int minScore) {
 		this.attribute = attribute;
 		this.setWin = setWin;
 		this.setLoose = setLoose;
 		this.addResult = addResult;
 		if(factor != 0)
 			this.factor = factor;
+		this.minScore = minScore;
 	}
 
 }
